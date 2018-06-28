3a729bd015bcbf32f210053cfb73c4bf6f6256de
jeremyphilemon <jeremyphilemon@outlook.com>
2018-06-28 21:00:59 +0530

    Actually fixed changelog api calls.

--- stats/api/serializers.py
+++ stats/api/serializers.py
@@ -4,16 +4,10 @@ from stats.models import Changelog
 
 
 class changelogSerializer(serializers.ModelSerializer):
-    url = serializers.SerializerMethodField(read_only=True)
-
     class Meta:
         model = Changelog
-        fields = ['url', 'pk', 'title', 'description', 'date', 'archived']
-        read_only_fields = ['url', 'pk']
-
-    def get_url(self, obj):
-        request = self.context.get("request")
-        return obj.get_api_url(request=request)
+        fields = ['pk', 'title', 'description', 'date', 'archived']
+        read_only_fields = ['pk']
 
     def validate_title(self, value):
         if(not value):
